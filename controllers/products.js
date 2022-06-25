76d8c23f5340003430c2c3010a4b1241b2b0c253
iykeslim <iykeslim01@gmail.com>
2022-06-25 23:59:25 +0100

    regex and numeric filters and wrap up on store api

--- controllers/products.js
+++ controllers/products.js
@@ -18,7 +18,7 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort, fields} = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query;
     const queryObject = {}
 
     if(featured){
@@ -31,7 +31,29 @@ const getAllProducts = async (req, res) => {
     if(name){
         queryObject.name = { $regex: name, $options: "i" };
     }
-    // console.log(queryObject)
+    if(numericFilters){
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte'
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`
+            )
+            // those ops can only be performed on numeric values so...
+            const options = ['price', 'rating'] // the only numeric values in our API
+            filters = filters.split(',').forEach((item) => {
+                const [field, operator, value] = item.split('-')
+                if(options.includes(field)){
+                    queryObject[field] = {[operator]: Number(value)}
+                }
+            })
+            }
+     console.log(queryObject)
     let result = Product.find(queryObject)
     // sort
     if(sort){
@@ -42,10 +64,18 @@ const getAllProducts = async (req, res) => {
     else{
         result = result.sort('createdAt')
     }
+
     if(fields){
         const fieldsList = fields.split(",").join(" ");
         result = result.select(fieldsList);
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+
+
     const products = await result
     res.status(200).json({products, nbHits: products.length})
 }
