d318938f1ad9050efdaae999ee0a44ef324b3630
iykeslim <iykeslim01@gmail.com>
2022-06-25 02:23:47 +0100

    filter search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,38 @@
-
+const Product = require('../models/product')
 
 
 
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing async errors package')
-    res.status(200).json({msg: 'products testing route'})
+    // reference only
+    const search = 'ab'
+    const products = await Product.find({
+        // 'i' implies case insensitive
+        name: {$regex: search, $options: 'i'}
+    })
+    res.status(200).json({products})
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'products route'})
+    const {featured, company, name} = req.query
+    const queryObject = {}
+
+    if(featured){
+        // in order to avoid error with queries without values in our api
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = { $regex: name, $options: "i" };
+    }
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 
 module.exports = {
     getAllProductsStatic,
     getAllProducts
-}
+}   
